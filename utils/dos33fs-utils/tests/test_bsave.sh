e5c5761fa0378472d92769d2429b7970b3163760
Jeff Quast <contact@jeffquast.com>
2023-08-16 14:36:10 -0400

    posix/dash/sh compatibility

--- utils/dos33fs-utils/tests/test_bsave.sh
+++ utils/dos33fs-utils/tests/test_bsave.sh
@@ -1,20 +1,20 @@
-#!/bin/sh
 echo "Testing dos33 BSAVE"
 
-function fail {
+fail() {
     echo "$1" >&2
     num_failed=$(($num_failed + 1))
 }
 num_failed=0
 
 tempfile=$(mktemp -t temp.XXXXXXXXXX)
-function run_test {
+run_test() {
     cmd_args=$1
-    expect_strings=("${@:2}")
+    shift
+    expect_strings="$@"
     # run and assert return code is zero
     ../dos33 -d ./test.dsk $cmd_args >"$tempfile" 2>&1 || fail "Command failed: $(cat "$tempfile")"
     # assert all expected strings are present in output
-    for expect_str in "${expect_strings[@]}"
+    for expect_str in $expect_strings
     do
         grep "$expect_str" "$tempfile" >/dev/null || fail "Command output missing expected string, '$expect_str'"
     done
@@ -25,7 +25,7 @@ run_test "BSAVE SINCOS EX1" "Local filename: SINCOS" "Apple filename: EX1"
 run_test "BSAVE -a 0x2000 SINCOS EX2" "Local filename: SINCOS" "Apple filename: EX2" "Address=8192"
 run_test "BSAVE -a 0x2000 -l 146 SINCOS EX3" "Local filename: SINCOS" "Apple filename: EX3" "Address=8192" "Length=146"
 rm "$tempfile"
-if [ $num_failed -gt 0 ]; then
+if [ "$num_failed" -gt 0 ]; then
     echo "Test failed. $num_failed tests failed."
     exit 1
 fi
